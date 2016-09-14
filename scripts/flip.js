30b1889afc0ff1bc19fa0f1442dad963f742dcc3
Diljit PR <diljit.p-r@hpe.com>
2016-09-15 01:16:30 +0530

    Things fixed in second version
    *Better Javascript code
    *Increased modularity
    *added better event handling
    *requirejs for AMD.

---
+++ scripts/flip.js
@@ -0,0 +1,95 @@
+define(["jquery"], function($) {
+
+    var sourceTextArea = "#sourceTextArea";
+    var destinationTextArea = "#outputText";
+    var flippedCharactersInUnicode = {
+        "a": "\u0250",
+        "b": "\u0071",
+        "c": "\u0254",
+        "d": "\u0254",
+        "d": "\u0070",
+        "e": "\u01DD",
+        "f": "\u025F",
+        "g": "\u0253",
+        "h": "\u0265",
+        "i": "\u0131",
+        "j": "\u027E",
+        "k": "\u029E",
+        "l": "\u006C",
+        "m": "\u026F",
+        "n": "\u0075",
+        "o": "\u006F",
+        "p": "\u0064",
+        "q": "\u0062",
+        "r": "\u0279",
+        "s": "\u0073",
+        "t": "\u0287",
+        "u": "\u006E",
+        "v": "\u028C",
+        "w": "\u028D",
+        "x": "\u0078",
+        "y": "\u028E",
+        "z": "\u007A",
+        "C": "\u10412",
+        "D": "\u0186",
+        "E": "\u15E1",
+        "F": "\u018E",
+        "G": "\u2132",
+        "H": "\u2141",
+        "I": "\u0048",
+        "J": "\u0049",
+        "K": "\u017F",
+        "L": "\u22CA",
+        "M": "\u2142",
+        "N": "\u0057",
+        "O": "\u004E",
+        "P": "\u004F",
+        "Q": "\u0500",
+        "R": "\u038C",
+        "S": "\u1D1A",
+        "T": "\u0053",
+        "U": "\u22A5",
+        "V": "\u2229",
+        "W": "\u039B",
+        "X": "\u004D",
+        "Y": "\u0058",
+        "Z": "\u2144",
+        " ": "\u0020",
+        "\n": "\u000D"
+    };
+
+
+
+    function flipText() {
+        var sourceText = $(sourceTextArea).val();
+        var sourceTextLength;
+        var invertedText = "";
+        var character;
+        var index;
+
+        if (sourceText) {
+            sourceTextLength = sourceText.length;
+            for (index = 0; index < sourceTextLength; index++) {
+                character = sourceText[index];
+                invertedText += flippedCharactersInUnicode[character];
+            }
+            displayInvertedText(invertedText);
+        } else
+            displayInvertedText("");
+    }
+
+    function displayInvertedText(invertedText) {
+        $(destinationTextArea).val(invertedText);
+    }
+
+    function initializeEvents() {
+        $(sourceTextArea).on("keyup", flipText);
+        console.log("Initiaized properly");
+    }
+
+    // return a public api to initialize events
+    return {
+        initialize: initializeEvents
+    };
+
+});
---
+++ scripts/init.js
@@ -0,0 +1,5 @@
+require(["jquery","flip"], function($,flip){
+    
+    //initialize the app by calling public api of flip.js fileCreatedDate
+    flip.initialize();
+});
